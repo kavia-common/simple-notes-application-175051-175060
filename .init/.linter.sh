b5e50da932e05561e63781703c1719c8fc058e16
Kavia AI <production@kavia.ai>
2025-11-21 09:47:30 +0000

    Checkpoint 1 - cg2fbf0649

---
+++ .init/.linter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/simple-notes-application-175051-175060/notes_backend
+dotnet build --no-restore -v quiet -nologo -consoleloggerparameters:NoSummary /p:TreatWarningsAsErrors=false
+LINT_EXIT_CODE=$?
+if [ $LINT_EXIT_CODE -ne 0 ]; then
+  exit 1
+fi
+
